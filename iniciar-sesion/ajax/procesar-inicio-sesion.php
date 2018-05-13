8da57ec121c6768edf80ae1bb659256b1bdb791b
paurv <ana.posadasr97@gmail.com>
2018-05-12 20:37:06 -0600

    Add files via upload

--- iniciar-sesion/ajax/procesar-inicio-sesion.php
+++ iniciar-sesion/ajax/procesar-inicio-sesion.php
@@ -1,6 +1,27 @@
-<?php 
+<?php
+session_start();
+include_once("../../class/class-conexion.php");
 
-	echo "\n \n Los datos recibidos fueron: \n";
-	echo "\n"."correo: ". $_POST['correo']."\n".
-		 "Clave: ". $_POST['clave'];
+ 
+ $conexion = new Conexion();
+  $sql = sprintf( "SELECT codigo_usr, nombre, apellido, clave, correo, tel, nacimiento, url_pef, genero FROM tbl_usuarios WHERE clave LIKE sha1('%s') AND correo LIKE '%s'",
+           $_POST["clave"],
+           $_POST["correo"]
+       );
+       
+       $resultado = $conexion->ejecutarConsulta($sql);
+       $respuesta = array();
+       if ($conexion->cantidadRegistros($resultado)>0){
+           $respuesta = $conexion->obtenerFila($resultado);
+           $respuesta["codigoResultado"] = 0;
+           $respuesta["mensajeResultado"] = "El usuario si existe";
+           $_SESSION["usr"] = $respuesta["correo"];
+           $_SESSION["psw"] = sha1($_POST["clave"]);
+       }else {
+           $respuesta["codigoResultado"] = 1;
+           $respuesta["mensajeResultado"] = "El usuario no existe";
+           session_destroy();
+	   }
+	  // echo $sql;
+       echo json_encode($respuesta);
 ?>
--- iniciar-sesion/js/controlador.js
+++ iniciar-sesion/js/controlador.js
@@ -1,18 +1,57 @@
+var validacion = false;
+
 function validarEmail(email) {
     var patron = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (patron.test(String(email.value).toLowerCase())) {
         $('#validar-correo').css('display', 'none');
+
+        validacion = true;
     } else {
         $('#validar-correo').css('display', 'inline');
+        $('#validacion-correo').css('display', 'none');
+        validacion = false;
 
     }
 }
 
 $('#btn-inicio').click(function() {
-    if ($('#clave-usuario').val() == "") {
-        $('#validacion-contrasena').css('display', 'inline');
+    // alert("esta vacio vez");
 
+    if ($('#txt-contrasena').val() == "" || $('#txt-correo').val() == "") {
+
+        $('#validacion-contrasena').css('display', 'inline'); //validacion-correo
+        $('#validacion-correo').css('display', 'inline');
+        validacion = false;
     } else {
         $('#validacion-contrasena').css('display', 'none');
+        $('#validacion-correo').css('display', 'none');
+        validacion = true;
+    }
+    if (validacion) {
+        mandar();
     }
 });
+
+function mandar() {
+
+    var parametros = 'correo=' + $("#txt-correo").val() + "& " +
+        'clave=' + $("#txt-contrasena").val();
+
+    //alert("Se enviara esto a procesar.php: " + parametros);
+
+    $.ajax({
+        url: 'ajax/procesar-inicio-sesion.php',
+        method: 'POST',
+        data: parametros,
+        dataType: "json",
+        success: function(data) {
+            console.log(data);
+            if (data.codigoResultado == 0) {
+                window.location.assign("http://localhost/google-plus/sesion-iniciada/index.php");
+            } else {
+                $('#encontro-usuario').css('display', 'inline');
+            }
+        }
+    });
+    //alert("Inicio de sesion Exitoso! " + data);
+}
