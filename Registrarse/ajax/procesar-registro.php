3769c5f1205fb00b1c90fb550539fe7615171001
paurv <ana.posadasr97@gmail.com>
2018-05-12 20:42:43 -0600

    Add files via upload

--- Registrarse/ajax/procesar-registro.php
+++ Registrarse/ajax/procesar-registro.php
@@ -1,17 +1,26 @@
 <?php
+session_start();
+include_once("../../class/class-conexion.php");
+ $conexion = new Conexion();
+ $respuesta= array();
 
-/*	echo "\n";
-	echo "Datos ingresados: ";
-	echo "\n"; */
-	echo "\n Los datos ingresados son: \n";
-	echo "\n"."Nombre: ". $_POST['nombre']."\n".
-		 "Apellido: ". $_POST['apellido']."\n".
-		 "Correo: ". $_POST['correo']."\n".
-		 "Clave: ". $_POST['clave']."\n".
-		 "Tel/Cel: ". $_POST['celular']."\n".
-		 "Fecha Nacimiento: ". $_POST['fecha']."\n".
-		 "Genero: ". $_POST['genero'];
+ $sql=sprintf("INSERT INTO tbl_usuarios( nombre,".
+ 			 "apellido, clave, correo, tel,".
+			 "nacimiento, url_pef, genero)".
+			 "VALUES ('%s','%s',sha1('%s'),'%s',%s,'%s','%s','%s')",
+			$conexion->antiInyeccion($_POST['nombre']),
+			$conexion->antiInyeccion($_POST['apellido']),
+			$conexion->antiInyeccion($_POST['clave']),
+			$conexion->antiInyeccion($_POST['correo']),
+			$conexion->antiInyeccion($_POST['celular']),
+			$conexion->antiInyeccion($_POST['fecha']),
+			$conexion->antiInyeccion($_POST['imagen']),
+			$conexion->antiInyeccion($_POST['genero']));
+ $conexion->ejecutarConsulta($sql);
 
-
-	//var_dump();
+ $_SESSION['usr'] = $_POST['correo'];
+ $_SESSION["psw"] = sha1($_POST['clave']);
+	$respuesta["codigoResultado"] = 0;
+	echo json_encode($respuesta);
+	//echo$sql;
 ?>
--- Registrarse/js/controlador.js
+++ Registrarse/js/controlador.js
@@ -1,57 +1,103 @@
+var correcto = false;
+
 function validarEmail(email) {
     var patron = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (patron.test(String(email.value).toLowerCase())){
-    	//email.classList.remove("is-invalid");
-    	//email.classList.add("is-valid");
-    	$('#validar-correo').css('display','none');
-    }
-    else{
-    	$('#validar-correo').css('display','inline');
-    	//email.classList.remove("is-valid");
-    	//email.classList.add("is-invalid");
+    if (patron.test(String(email.value).toLowerCase())) {
+        $('#validar-correo').css('display', 'none');
+        correcto = true;
+
+    } else {
+        $('#validar-correo').css('display', 'inline');
+        $('#validacion-correo').css('display', 'none');
+        correcto = false;
     }
 }
 
 
-/*var validando = function(){
-	validacion('txt-nombre');
-	validacion('txt-contrasena');
-	validacion('txt-celular');
-	validacion('txt-fecha-nacimiento');
-}
-*/
+
 
 
 $("#btn-registrarse").click(function(event) {
-	//alert("hola perros");
-	if ($('#txt-nombre').val()==""){
-		$('#validacion-nombre').css('display','inline');
 
-	} else {
-		$('#validacion-nombre').css('display','none');
-	}
+    if ($('#txt-nombre').val() == "") {
+        $('#validacion-nombre').css('display', 'inline');
+        correcto = false;
+    } else {
+        $('#validacion-nombre').css('display', 'none');
+        correcto = true;
+    }
+
+    if ($('#txt-correo').val() == "") {
+        $('#validacion-correo').css('display', 'inline');
+        correcto = false;
+    } else {
+        $('#validacion-correo').css('display', 'none');
+        correcto = true;
+    }
 
 
-	if ($('#txt-apellido').val()==""){
-		$('#validacion-apellido').css('display','inline');
 
-	} else {
-		$('#validacion-apellido').css('display','none');
-	}
+    if ($('#txt-apellido').val() == "") {
+        $('#validacion-apellido').css('display', 'inline');
+        correcto = false;
 
-	if ($('#txt-contrasena').val()==""){
-		$('#validacion-contrasena').css('display','inline');
+    } else {
+        $('#validacion-apellido').css('display', 'none');
+        correcto = true;
+    }
 
-	} else {
-		$('#validacion-contrasena').css('display','none');
-	}
+    if ($('#txt-contrasena').val() == "") {
+        $('#validacion-contrasena').css('display', 'inline');
+        correcto = false;
+    } else {
+        $('#validacion-contrasena').css('display', 'none');
+        correcto = true;
+    }
 
-	if ($('#txt-fecha-nacimiento').val()==""){
-		$('#validacion-fecha-nacimiento').css('display','inline');
+    if ($('#txt-celular').val() == "") {
+        $('#validacion-celular').css('display', 'inline');
+        correcto = false;
+    } else {
+        $('#validacion-celular').css('display', 'none');
+        correcto = true;
+    }
 
-	} else {
-		$('#validacion-fecha-nacimiento').css('display','none');
-	}
-	
+    if ($('#txt-fecha-nacimiento').val() == "") {
+        $('#validacion-fecha-nacimiento').css('display', 'inline');
+        correcto = false;
+    } else {
+        $('#validacion-fecha-nacimiento').css('display', 'none');
+        correcto = true;
+    }
+    if (correcto)
+        mandar();
+
 });
 
+function mandar() {
+    var fechaN = document.getElementById("txt-fecha-nacimiento").value;
+    var parametros = 'nombre=' + $("#txt-nombre").val() +
+        "&" + 'apellido=' + $("#txt-apellido").val() +
+        "&" + 'correo=' + $("#txt-correo").val() +
+        "&" + 'clave=' + $("#txt-contrasena").val() +
+        "&" + 'celular=' + $("#txt-celular").val() +
+        "&" + 'fecha=' + fechaN +
+        "&" + 'genero= ' + $("#slc-genero").val() +
+        "&" + 'imagen= ' + $("#slc-url-img").val();
+
+    alert("Se enviara esto a procesar.php: " + parametros);
+
+    $.ajax({
+        url: 'ajax/procesar-registro.php',
+        method: 'POST',
+        dataType: "json",
+        data: parametros,
+        success: function(respuesta) {
+            console.log(respuesta);
+            if (respuesta.codigoResultado == 0)
+                window.location.assign("http://localhost/google-plus/sesion-iniciada/index.php");
+        }
+    });
+
+
+}
